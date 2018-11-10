79d9843d2149d6cfcc3d6e8bc6a182676dd8ba68
Rob <robert.vitali@wustl.edu>
2018-11-09 21:04:38 -0600

    First Commit, Project File

---
+++ WUNews_Final_Project/WUNews_Final_ProjectTests/WUNews_Final_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  WUNews_Final_ProjectTests.swift
+//  WUNews_Final_ProjectTests
+//
+//  Created by Robert on 11/9/18.
+//  Copyright © 2018 Robert Vitali. All rights reserved.
+//
+
+import XCTest
+@testable import WUNews_Final_Project
+
+class WUNews_Final_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
