4cb8e3e9cb2b5431085d5b6f1c3a946a9fdf7e18
Rob <robert.vitali@wustl.edu>
2018-11-15 17:15:15 -0600

    Updated File

---
+++ TasksApp_Final_Project/Pods/Target Support Files/Pods-TasksApp_Final_ProjectTests/Pods-TasksApp_Final_ProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TasksApp_Final_ProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_TasksApp_Final_ProjectTests
+@end
---
+++ TasksApp_Final_Project/TasksApp_Final_Project/SignInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignInViewController.swift
+//  TasksApp_Final_Project
+//
+//  Created by Robert on 11/15/18.
+//  Copyright © 2018 Robert Vitali. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+import Firebase
+
+class SignInViewController: UIViewController, GIDSignInUIDelegate {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
