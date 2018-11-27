b42d2a65b98e719e8fe85f19b17b4af33bef6fb0
Kelly Miller <kellymiller@kellys-mbp.attlocal.net>
2018-11-27 00:04:30 -0600

    Set Up Today View

---
+++ TasksApp_Final_Project/TasksApp_Final_Project/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  TasksApp_Final_Project
+//
+//  Created by Spencer Blackwwod on 11/26/18.
+//  Copyright © 2018 Robert Vitali. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.3
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 1.5
+    }
+    
+}
---
+++ TasksApp_Final_Project/TasksApp_Final_Project/ExpandingTableView.swift
@@ -0,0 +1,34 @@
+//
+//  ExpandingTableView.swift
+//  TasksApp_Final_Project
+//
+//  Created by Spencer Blackwood on 11/26/18.
+//  Copyright © 2018 Robert Vitali. All rights reserved.
+//
+//  This is a UITableView that will expand its height to display all cells.
+
+import UIKit
+
+class ExpandingTableView: UITableView {
+    
+    override func draw(_ rect: CGRect) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.3
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 1.5
+    }
+    
+    //  https://stackoverflow.com/questions/35876003/expand-uitableview-to-show-all-cells-in-stack-view
+    override var contentSize:CGSize {
+        didSet {
+            self.invalidateIntrinsicContentSize()
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        self.layoutIfNeeded()
+        return CGSize(width: UIViewNoIntrinsicMetric, height: contentSize.height)
+    }
+    
+}
