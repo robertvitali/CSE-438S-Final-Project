836b5567301f2dc2a8b6ffa8425d34ccd883c2f3
Rob <robert.vitali@wustl.edu>
2018-12-02 20:18:08 -0600

    coredata

---
+++ TasksApp_Final_Project/TasksApp_Final_Project/CoreData/Folders+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Folders+CoreDataClass.swift
+//  TasksApp_Final_Project
+//
+//  Created by Robert on 12/2/18.
+//  Copyright © 2018 Robert Vitali. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Folders: NSManagedObject {
+
+}
